31922c2ccdfd3eacf6a469d48bcc4a52bb531e43
Sriram Ravi <sriram.ravi@hm.com>
2025-09-03 17:36:46 +0530

    Add project files.

---
+++ Infrastructure/Config/ProductConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Config
+{
+    public class ProductConfiguration: IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(x => x.Price).HasColumnType("decimal(18,2)");
+        }
+    }
+}
